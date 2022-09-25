f3a44c63077b875d86b21d892e263b483ba1e8db
rodavid20 <rodavid20@users.noreply.github.com>
2022-09-25 14:54:02 +0530

    restructured

---
+++ DatabaseDemo/app/src/main/java/com/rodavid20/filedemo/datamodel/Student.java
@@ -0,0 +1,36 @@
+package com.rodavid20.filedemo.datamodel;
+
+public class Student {
+    private int id;
+    private String name;
+    private String departmentName;
+
+    public Student(){
+    }
+
+    public Student(int id, String name, String departmentName){
+        this.id = id;
+        this.name = name;
+        this.departmentName = departmentName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+}
