7906040d933348911c2a89eec5f6a2fdecd07460
rodavid20 <rodavid20@users.noreply.github.com>
2022-11-06 12:32:29 +0530

    initial

---
+++ SensorDemo/app/src/main/java/com/rodavid20/sensordemo/MainActivity.java
@@ -0,0 +1,53 @@
+package com.rodavid20.sensordemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity
+    implements SensorEventListener {
+    SensorManager sensorManager;
+    Sensor accSensor;
+    TextView tvMessage;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvMessage = findViewById(R.id.tvMessage);
+        sensorManager =
+                (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, accSensor,
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        tvMessage.setText("x" + sensorEvent.values[0]
+                + "y" + sensorEvent.values[1]
+                + "z" + sensorEvent.values[2]);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+}
